eb56b15a94c0c036281e6b4c6a8584c78dfd2397
acsbrito <anac.brito@yahoo.com.br>
2021-02-13 17:40:18 -0300

    criação relacionamento

---
+++ src/Modules/InstaGama.Domain/Entities/Friends.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaGama.Domain.Entities
+{
+    public class Friends
+    {
+        public Friends(int userId, int userFriendId, bool pendency)
+        {
+            UserId = userId;
+            UserFriendId = userFriendId;
+            Pendency = pendency;
+        }
+        public int Id { get; private set; }
+        public int UserId { get; private set; }
+        public int UserFriendId { get; private set; }
+        public bool Pendency { get; private set; }
+    }
+}
+
+
+
+
+
---
+++ src/Modules/InstaGama.Domain/Interfaces/IFriendsRepository.cs
@@ -0,0 +1,15 @@
+﻿using InstaGama.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstaGama.Domain.Interfaces
+{
+    interface IFriendsRepository
+    {
+        Task<int> InsertAsync(Friends friends);
+
+        Task<List<Friends>> GetFriendsByUserIdAsync(int userId);
+    }
+}
