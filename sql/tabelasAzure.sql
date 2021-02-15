e484d10ef9343ae7b90f3b7c94f90452c20d5e06
KellyCarvalho <18kellycarvalho@gmail.com>
2021-02-15 10:48:46 -0300

    adicionando espaço para foto em postagem

--- sql/tabelasAzure.sql
+++ sql/tabelasAzure.sql
@@ -1,5 +1,11 @@
 ﻿
 
+
+USE GRUPO3;
+--Criação do Database
+
+
+--Criação das tabelas
 CREATE TABLE dbo.Genero (
    Id int IDENTITY(1,1) NOT NULL,
    Descricao varchar(50) NOT NULL,
@@ -26,6 +32,7 @@ CREATE TABLE dbo.Postagem (
    Id int IDENTITY(1,1) NOT NULL,
    UsuarioId int NOT NULL,
    Texto varchar(250) NOT NULL,
+   Foto varchar(max),
    Criacao DateTime NOT NULL,
    CONSTRAINT PK_Postagem_Id PRIMARY KEY CLUSTERED (Id)
 )
@@ -68,4 +75,9 @@ ALTER TABLE dbo.Curtidas
    ADD CONSTRAINT FK_Curtidas_Postagem FOREIGN KEY (PostagemId)
       REFERENCES dbo.Postagem (Id)
 
-	  INSERT INTO Genero VALUES ('Feminino'); 
+INSERT INTO Genero VALUES ('Masculino'); 
+INSERT INTO Genero VALUES ('Feminino'); 
+
+
+
+
--- src/Modules/InstaGama.Domain/Entities/Postage.cs
+++ src/Modules/InstaGama.Domain/Entities/Postage.cs
@@ -14,7 +14,16 @@ namespace InstaGama.Domain.Entities
             Created = DateTime.Now;
         }
 
-   
+        public Postage( string text, int userId, string photo)
+        {
+            
+            Text = text;
+            UserId = userId;
+            Photo = photo;
+            Created = DateTime.Now;
+        }
+
+
 
         public Postage(int id,
                         string text,
@@ -26,15 +35,40 @@ namespace InstaGama.Domain.Entities
             UserId = userId;
             Created = created;
         }
+        public Postage(int id,
+                       string text,
+                       string photo,
+                       int userId,
+                       DateTime created)
+        {
+            Id = id;
+            Text = text;
+            Photo = photo;
+            UserId = userId;
+            Created = created;
+        }
+
+
 
         public int Id { get; private set; }
         public int UserId { get; private set; }
         public string Text { get; private set; }
+        public string Photo { get; private set; }
         public DateTime Created { get; private set; }
 
         public void SetId(int id)
         {
             Id = id;
+        }
+
+        public bool IsValid()
+        {
+            if(string.IsNullOrEmpty(UserId.ToString())||
+                string.IsNullOrEmpty(Text)){
+                return false;
+            }
+            return true;
+
         }
 
     
